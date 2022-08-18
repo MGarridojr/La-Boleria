09f73779dce27fd45cdcd47a294d6bdce19c1d59
Driven Education <marcio.garrido.jr@gmail.com>
2022-08-18 14:14:51 -0300

    feat: add a middleware on cakes post route

--- routes/cakesRouter.js
+++ routes/cakesRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { cakesnomepraperguntarfelipe } from "../controllers/cakesController.js";
+import { validateSchema } from "../middlewares/schemaValidator.js";
+import cakesSchema from "../schemas/cakesSchema.js";
 
 
 const cakesRouter = Router();
 
-cakesRouter.post("/cakes", cakesnomepraperguntarfelipe )
+cakesRouter.post("/cakes",validateSchema(cakesSchema) ,cakesnomepraperguntarfelipe )
 
 export default cakesRouter
